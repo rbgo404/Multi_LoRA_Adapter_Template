5bebba8a17fd63f69e284036a333f6095b59553c
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-20 17:09:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,12 @@ class InferlessPythonModel:
     def infer(self, inputs):
         prompt = inputs["prompt"]
         adapter_name = inputs.pop("adapter_name")
-
-        print(self.model.active_adapter)
-        if (self.model.active_adapter) != adapter_name:
-            self.model.set_adapter(adapter_name)
-
         temperature = inputs.get("temperature",0.7)
         repetition_penalty = float(inputs.get("repetition_penalty",1.18))
         max_new_tokens = inputs.get("max_new_tokens",128)
+        
+        if (self.model.active_adapter) != adapter_name:
+            self.model.set_adapter(adapter_name)
 
         model_input = self.tokenizer(prompt, return_tensors="pt").to("cuda")
         result = self.tokenizer.decode(self.model.generate(**model_input,temperature=temperature, max_new_tokens=max_new_tokens, repetition_penalty=repetition_penalty)[0], skip_special_tokens=True)
